15ae6254e53e4f93be1ddd657fdcf766b4a6a410
nand chan <nandykins@gmail.com>
2011-11-05 11:34:12 +0100

    rebuilt release

--- implementations/nand/Test/Program.cs
+++ implementations/nand/Test/Program.cs
@@ -11,38 +11,12 @@ namespace Test
         static void Main(string[] args)
         {
             const string test = @"
-
->implying foo isn't >function(x) {
-     x TIER: {
-         >mfw ""true"";
-         >forever alone.jpg
-     } 100% accurate
-
-     >mfw ""false"";
-     gb2 -1
- }
-
->foo false;
-
->mfw >>function(x) >add x x; 5;;
-
 >implying factorial isn't >function(n)
     >is n 0; TIER: 1
     SHIT TIER: >mul n >this >sub n 1;;;
 100% accurate
 
-inane >mfw >factorial 5;;
-
->>function(x)
-   >is x 0; TIER:
-     forever alone
-   SHIT TIER: {
-     >mfw x;
-     >this >sub x 1;;
-     >mfw x;
-   } 100% accurate
- 5;
-
+>mfw >factorial 20;;
 ";
 
             Value v = Interpreter.Run(test);
